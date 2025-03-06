91ee5cea396b25420d094b32b9a002cf0ef2b24f
AlexWebb03 <alexanderwebb03@gmail.com>
2025-03-06 16:32:31 +0000

    register ant environment

--- brax/envs/fd/__init__.py
+++ brax/envs/fd/__init__.py
@@ -1,11 +1,13 @@
-from brax.envs.fd import finger, linear, inverted_pendulum, pusher
+from brax.envs.fd import finger, linear, inverted_pendulum, pusher, shadow_hand, ant
 from brax.envs.fd.fd_env import FDEnv
 
 _envs = {
     'finger': finger.Finger,
     'linear': linear.Linear,
     'inverted_pendulum': inverted_pendulum.InvertedPendulum,
-    'pusher': pusher.Pusher
+    'pusher': pusher.Pusher,
+    'shadow_hand': shadow_hand.ShadowHand,
+    'ant': ant.Ant,
 }
 
 def get_environment(env_name: str, **kwargs) -> FDEnv:
